54f5f1c1084aae3c73fac5f5024382d444a2fcfe
Magnus Winkel Pedersen <mwpe16@student.aau.dk>
2019-11-12 14:31:15 +0100

    api'en tager højde for 5 measurements
    og tminstant og tmobject er blevet til strings, som følger iso 8601 standarden

--- IO.Swagger/Models/Observation.cs
+++ IO.Swagger/Models/Observation.cs
@@ -11,14 +11,14 @@ namespace IO.Swagger.Models
     /// </summary>
     public class Observation
     {
-        public TmObject TmObject;
-        public TmInstant TmInstant;
+        public string PhenomenonTime;
+        public string ResultTime;
         public double Result;
 
-        public Observation(TmObject tmObject, TmInstant tmInstant, double result)
+        public Observation(string phenomenonTime, string resultTime, double result)
         {
-            TmObject = tmObject;
-            TmInstant = tmInstant;
+            PhenomenonTime = phenomenonTime;
+            ResultTime = resultTime;
             Result = result;
         }
     }
