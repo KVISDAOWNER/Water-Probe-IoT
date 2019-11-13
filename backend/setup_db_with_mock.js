8eec2cfb9a98e4b281781b54e7dd2b044bee3aab
KVISDAOWNER <krijen16@student.aau.dk>
2019-11-13 12:37:55 +0100

    removed invalid mock location

--- backend/setup_db_with_mock.js
+++ backend/setup_db_with_mock.js
@@ -12,9 +12,6 @@ db.createCollection('Phosphorus_1D95A5');
 
     
 db.Probe.insert({_id : '1D95A5', description : 'Lorem ipsum dol', properties : {}, locations: [{locationReference: "locRef", locationTime: "2014-12-31T11:59:59.00+08:00"}], attachedSensors: [{refToSensor: "MJKDZ", description: "Water Turbidity"},{refToSensor: "DS18B20", description: "Water Temperatur"},{refToSensor: "PH4502C", description: "Water pH"},{refToSensor: "Nitrogen", description: "Water Nitrogen"}, {refToSensor: "Phosphorus", description: "Water Phosphorus"}]});
-
-db.Location.insert({description : 'Lorem ipsum dolo', 'properties' : {}, encodingType: "Some encodingType", location: 'Coordinates here'});
-
 db.Sensor.insert({_id : 'MJKDZ', description : 'Turbidity Sensor', encodingType: "Some encodingType", metadata: {}, unitOfMeasurement: "NTU", observedPropertyRef:"WaterTurbidity"});
 db.Sensor.insert({_id : 'DS18B20', description : 'Temperature Sensor', encodingType: "Some encodingType", metadata: {}, unitOfMeasurement: "°C", observedPropertyRef:"WaterTemperature"});
 db.Sensor.insert({_id : 'PH4502C', description : 'pH Sensor', encodingType: "Some encodingType", metadata: {}, unitOfMeasurement: "pH", observedPropertyRef:"WaterPH"});
@@ -84,6 +81,7 @@ db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-08T11:59:59.00+08:00", resu
 db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-09T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "90"})
 db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-10T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "00"})
 db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-11T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "10"})
+
 db.ObservedProperty.insert({_id: "WaterPH", definition: "some URI", description: "The pH of water"});
 db.ObservedProperty.insert({_id: "WaterNitrogen", definition: "some URI", description: "The Nitrogen concentration of water"});
 db.ObservedProperty.insert({_id: "WaterPhosphorus", definition: "some URI", description: "The Phosphorus concentration of water"});
