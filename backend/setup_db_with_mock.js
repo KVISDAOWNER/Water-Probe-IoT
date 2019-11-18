8abaeb6083a0f85947ba416ee8835fa605b7e596
KVISDAOWNER <krijen16@student.aau.dk>
2019-11-18 14:20:20 +0100

    fixed setup database script to not have duplicate resulttime date

--- backend/setup_db_with_mock.js
+++ backend/setup_db_with_mock.js
@@ -27,59 +27,59 @@ db.ObservedProperty.insert({_id: "WaterTemperature", definition: "some URI", des
 db.ObservedProperty.insert({_id: "WaterTurbidity", definition: "some URI", description: "The turbidity of water"});
 
 db.Temperature_1D95A5.insert({phenomenonTime: "2014-12-31T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "12"});
-db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-01T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "13"});
-db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-02T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "14"});
-db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-03T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "15"});
-db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-04T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "16"});
-db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-05T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "17"});
-db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-06T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "18"});
-db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-07T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "19"});
-db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-08T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "20"});
-db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-09T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "21"});
-db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-10T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "22"});
-db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-11T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "23"});
-db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-12T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "24"});
+db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-01T11:59:59.00+08:00", resultTime: "2014-13-01T11:59:59.00+08:00", result: "13"});
+db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-02T11:59:59.00+08:00", resultTime: "2014-13-02T11:59:59.00+08:00", result: "14"});
+db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-03T11:59:59.00+08:00", resultTime: "2014-13-03T11:59:59.00+08:00", result: "15"});
+db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-04T11:59:59.00+08:00", resultTime: "2014-13-04T11:59:59.00+08:00", result: "16"});
+db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-05T11:59:59.00+08:00", resultTime: "2014-13-05T11:59:59.00+08:00", result: "17"});
+db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-06T11:59:59.00+08:00", resultTime: "2014-13-06T11:59:59.00+08:00", result: "18"});
+db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-07T11:59:59.00+08:00", resultTime: "2014-13-07T11:59:59.00+08:00", result: "19"});
+db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-08T11:59:59.00+08:00", resultTime: "2014-13-08T11:59:59.00+08:00", result: "20"});
+db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-09T11:59:59.00+08:00", resultTime: "2014-13-09T11:59:59.00+08:00", result: "21"});
+db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-10T11:59:59.00+08:00", resultTime: "2014-13-10T11:59:59.00+08:00", result: "22"});
+db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-11T11:59:59.00+08:00", resultTime: "2014-13-11T11:59:59.00+08:00", result: "23"});
+db.Temperature_1D95A5.insert({phenomenonTime: "2014-13-12T11:59:59.00+08:00", resultTime: "2014-13-12T11:59:59.00+08:00", result: "24"});
 
 db.Turbidity_1D95A5.insert({phenomenonTime: "2014-12-31T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "300"})
-db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-01T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "310"})
-db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-02T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "320"})
-db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-03T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "330"})
-db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-04T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "340"})
-db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-05T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "350"})
-db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-06T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "360"})
-db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-07T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "370"})
-db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-08T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "380"})
-db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-09T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "390"})
-db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-10T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "400"})
-db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-11T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "410"})
+db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-01T11:59:59.00+08:00", resultTime: "2014-13-01T11:59:59.00+08:00", result: "310"})
+db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-02T11:59:59.00+08:00", resultTime: "2014-13-02T11:59:59.00+08:00", result: "320"})
+db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-03T11:59:59.00+08:00", resultTime: "2014-13-03T11:59:59.00+08:00", result: "330"})
+db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-04T11:59:59.00+08:00", resultTime: "2014-13-04T11:59:59.00+08:00", result: "340"})
+db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-05T11:59:59.00+08:00", resultTime: "2014-13-05T11:59:59.00+08:00", result: "350"})
+db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-06T11:59:59.00+08:00", resultTime: "2014-13-06T11:59:59.00+08:00", result: "360"})
+db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-07T11:59:59.00+08:00", resultTime: "2014-13-07T11:59:59.00+08:00", result: "370"})
+db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-08T11:59:59.00+08:00", resultTime: "2014-13-08T11:59:59.00+08:00", result: "380"})
+db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-09T11:59:59.00+08:00", resultTime: "2014-13-09T11:59:59.00+08:00", result: "390"})
+db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-10T11:59:59.00+08:00", resultTime: "2014-13-10T11:59:59.00+08:00", result: "400"})
+db.Turbidity_1D95A5.insert({phenomenonTime: "2014-13-11T11:59:59.00+08:00", resultTime: "2014-13-11T11:59:59.00+08:00", result: "410"})
 
 
 db.Temperature_1D95A6.insert({phenomenonTime: "2014-12-31T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "9"});
-db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-01T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "1"});
-db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-02T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "14"});
-db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-03T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "25"});
-db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-04T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "36"});
-db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-05T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "7"});
-db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-06T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "8"});
-db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-07T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "29"});
-db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-08T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "25"});
-db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-09T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "26"});
-db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-10T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "27"});
-db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-11T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "25"});
-db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-12T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "26"});
+db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-01T11:59:59.00+08:00", resultTime: "2014-13-01T11:59:59.00+08:00", result: "1"});
+db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-02T11:59:59.00+08:00", resultTime: "2014-13-02T11:59:59.00+08:00", result: "14"});
+db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-03T11:59:59.00+08:00", resultTime: "2014-13-03T11:59:59.00+08:00", result: "25"});
+db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-04T11:59:59.00+08:00", resultTime: "2014-13-04T11:59:59.00+08:00", result: "36"});
+db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-05T11:59:59.00+08:00", resultTime: "2014-13-05T11:59:59.00+08:00", result: "7"});
+db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-06T11:59:59.00+08:00", resultTime: "2014-13-06T11:59:59.00+08:00", result: "8"});
+db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-07T11:59:59.00+08:00", resultTime: "2014-13-07T11:59:59.00+08:00", result: "29"});
+db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-08T11:59:59.00+08:00", resultTime: "2014-13-08T11:59:59.00+08:00", result: "25"});
+db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-09T11:59:59.00+08:00", resultTime: "2014-13-09T11:59:59.00+08:00", result: "26"});
+db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-10T11:59:59.00+08:00", resultTime: "2014-13-10T11:59:59.00+08:00", result: "27"});
+db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-11T11:59:59.00+08:00", resultTime: "2014-13-11T11:59:59.00+08:00", result: "25"});
+db.Temperature_1D95A6.insert({phenomenonTime: "2014-13-12T11:59:59.00+08:00", resultTime: "2014-13-12T11:59:59.00+08:00", result: "26"});
 
 db.Turbidity_1D95A6.insert({phenomenonTime: "2014-12-31T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "30"})
-db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-01T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "30"})
-db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-02T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "20"})
-db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-03T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "30"})
-db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-04T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "40"})
-db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-05T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "50"})
-db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-06T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "60"})
-db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-07T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "70"})
-db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-08T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "80"})
-db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-09T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "90"})
-db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-10T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "01"})
-db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-11T11:59:59.00+08:00", resultTime: "2014-12-31T11:59:59.00+08:00", result: "10"})
+db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-01T11:59:59.00+08:00", resultTime: "2014-13-01T11:59:59.00+08:00", result: "30"})
+db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-02T11:59:59.00+08:00", resultTime: "2014-13-02T11:59:59.00+08:00", result: "20"})
+db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-03T11:59:59.00+08:00", resultTime: "2014-13-03T11:59:59.00+08:00", result: "30"})
+db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-04T11:59:59.00+08:00", resultTime: "2014-13-04T11:59:59.00+08:00", result: "40"})
+db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-05T11:59:59.00+08:00", resultTime: "2014-13-05T11:59:59.00+08:00", result: "50"})
+db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-06T11:59:59.00+08:00", resultTime: "2014-13-06T11:59:59.00+08:00", result: "60"})
+db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-07T11:59:59.00+08:00", resultTime: "2014-13-07T11:59:59.00+08:00", result: "70"})
+db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-08T11:59:59.00+08:00", resultTime: "2014-13-08T11:59:59.00+08:00", result: "80"})
+db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-09T11:59:59.00+08:00", resultTime: "2014-13-09T11:59:59.00+08:00", result: "90"})
+db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-10T11:59:59.00+08:00", resultTime: "2014-13-10T11:59:59.00+08:00", result: "01"})
+db.Turbidity_1D95A6.insert({phenomenonTime: "2014-13-11T11:59:59.00+08:00", resultTime: "2014-13-11T11:59:59.00+08:00", result: "10"})
 
 db.ObservedProperty.insert({_id: "WaterPH", definition: "some URI", description: "The pH of water"});
 db.ObservedProperty.insert({_id: "WaterNitrogen", definition: "some URI", description: "The Nitrogen concentration of water"});
