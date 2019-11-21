d35ce8acfcc79b66992b65029bbe7c3c2d188318
Bjarke Hilmer Møller <bmalle16@student.aau.dk>
2019-11-21 10:29:38 +0100

    able to post simulated MI predictions

--- mi_prog/MiSimulator/MiSimulator/Program.cs
+++ mi_prog/MiSimulator/MiSimulator/Program.cs
@@ -1,21 +1,118 @@
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
-namespace MiSimulator
+namespace MIapp
 {
+
+    class Observation
+    {
+        public Observation(string PhenomenonTime, string ResultTime, double Result)
+        {
+            this.PhenomenonTime = PhenomenonTime;
+            this.ResultTime = ResultTime;
+            this.Result = Result;
+        }
+        public string PhenomenonTime { get; set; }
+        public string ResultTime { get; set; }
+        public double Result { get; set; }
+    }
+
+    class Prediction
+    {
+        public Prediction(string Probe, string Sensor, List<Observation> Values)
+        {
+            this.Probe = Probe;
+            this.Sensor = Sensor;
+            this.Values = Values;
+        }
+        public string Probe { get; set; }
+
+        public string Sensor { get; set; }
+
+        public List<Observation> Values { get; set; }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.WriteLine("Type the id for the probe you want to make predictions for");
+            string id = Console.ReadLine();
+
+
+            Communicator c = new Communicator(id);
+            c.run();
+        }
+    }
+
+    class Communicator
+    {
+        public Communicator(string probeId)
+        {
+            iD = probeId;
+        }
+
+        private const string URL = "http://localhost:50352/";
+        private const string GET = "probe?=";
+        private const string POST = "probes";
+        private string iD;
+        private const string PREDICTIONCATEGORY = "MI_PhosphorusSER_";
+
+        //The Run method both gets the measurements for a specific probe and it does also post values which have been altered
+        public void run()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+            List<Observation> results = new List<Observation>();
+
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync(GET + iD).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body.
+                var resp = response.Content.ReadAsStringAsync().Result;
+                var dataObjects = JsonConvert.DeserializeObject<List<Tuple<string, List<Observation>>>>(JsonConvert.DeserializeObject<string>(response.Content.ReadAsStringAsync().Result));
+                foreach (var lst in dataObjects)
+                {
+                    foreach (var tup in lst.Item2)
+                    {
+                        if ((lst.Item1.StartsWith("MI_")))
+                            continue;
+
+                        results.Add(new Observation(tup.PhenomenonTime, tup.ResultTime, tup.Result + 10));
+                    }
+                }
+
+                Prediction p = new Prediction(iD, PREDICTIONCATEGORY, results);
+
+                try
+                {
+                    var responser = client.PostAsJsonAsync(POST, p).Result;
+                    Console.WriteLine("s");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                client.Dispose();
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+            client.Dispose();
         }
     }
 }
+
