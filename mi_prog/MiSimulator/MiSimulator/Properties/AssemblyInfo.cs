686ac04cf48b8fc7bfa3b74beba77d426b966fd2
Bjarke Hilmer Møller <bmalle16@student.aau.dk>
2019-11-21 10:11:43 +0100

    fix of folder structure

--- mi_prog/MiSimulator/MiSimulator/Properties/AssemblyInfo.cs
+++ mi_prog/MiSimulator/MiSimulator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MIapp")]
+[assembly: AssemblyTitle("MiSimulator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MIapp")]
+[assembly: AssemblyProduct("MiSimulator")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5aeba1c9-ecac-4ec2-a79a-d02b8d5f5b40")]
+[assembly: Guid("8ef3fe12-728f-4345-bf31-8981c8919441")]
 
 // Version information for an assembly consists of the following four values:
 //
