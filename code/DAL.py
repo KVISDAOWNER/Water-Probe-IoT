8cfd0cd8b13e947d2b639ae5b802808ec7564fb0
Jacob Søndergaard <24315940+Jacob6565@users.noreply.github.com>
2019-11-22 13:38:55 +0100

    all should be working

--- code/DAL.py
+++ code/DAL.py
@@ -1,25 +1,74 @@
 # Data access layer
 import openapi_client
-
-def get_thing(id):
-    mockLoc = (1,2) # X Y
-    mockPh = [5.3, 6.5, 7.2, 5.7, 6.1, 8.1, 6.8, 6.9]
-    mockTemp = [20.3, 19.7, 21.1, 20.1, 19.8, 20.0, 20.6, 22.5]
-    mockTime = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    mockBat = [10, 20, 30, 15, 12, 10, 7, 18]
-    data_dict = {"ph" : mockPh,
-                 "temp" : mockTemp,
-                 "time" : mockTime,
-                 "loc" : mockLoc,
-                 "bat": mockBat}
-    return data_dict
+import json
+thingapi = openapi_client.ThingApi()
+datastreamapi = openapi_client.DatastreamApi()
+observationapi = openapi_client.ObservationApi()
 
 
-def get_id_of_things():
-    return [1,2,3,4]
+def get_names_of_things():
+    thingapi.api_client.configuration.host = '130.225.57.56:51099'
+    result = thingapi.api_client.call_api('/Things', 'GET', response_type='list[Thing]')
+    probes = result[0]
+    names = []
+    for probe in probes:
+        names.append(probe.name)
+
+    return names
 
 
-if __name__ == "__main__":
-    result = get_id_of_things()
-    i = 0
+def get_latitudes(probes):
+    lat = []
+    for probe in probes:
+        if probe.location is None:
+            lat.append("0.0000")
+        else:
+            lat.append(probe.location.location["lat"])
+    return lat
 
+
+def get_things_objects():
+    api_instance = openapi_client.ThingApi()
+    api_instance.api_client.configuration.host = '130.225.57.56:51099'
+    result = api_instance.api_client.call_api('/Things', 'GET', response_type='list[Thing]')
+    probes = result[0]
+    return probes
+
+
+def get_longitudes(probes):
+    long = []
+    for probe in probes:
+        if probe.location is None:
+            long.append("0.0000")
+        else:
+            long.append(probe.location.location["long"])
+    return long
+
+
+def get_descriptions(probes):
+    desc = []
+    for probe in probes:
+        if probe.description is None:
+            desc.append("No Description")
+        else:
+            desc.append(probe.description)
+    return desc
+
+
+def get_observations_of_datastreams(datastreams):
+    # datastreams er en liste af datastream navne
+    observations = []
+    for datastream in datastreams:
+        obs = observationapi.api_client.request('GET', '130.225.57.56:51099/Observations',
+                                                query_params=[('dataStreamRef', datastream)])
+        observations.append(json.loads(obs.data))
+    return observations
+
+
+def get_datastreams_of_things(thingnames):
+    datastreams = []
+    for thingname in thingnames:
+        datastreams.append(json.loads(datastreamapi.api_client.request('GET', '130.225.57.56:51099/dataStreams',
+                                                                       query_params=[('thingRef', thingname)]).data))
+
+    return datastreams
