e46353701cd1b9d2d89cb22ea6e7305e0a5e3203
Bjarke Hilmer Møller <bmalle16@student.aau.dk>
2019-11-27 09:47:12 +0100

    you can make requests asynchronously to check the API

---
+++ RequestBomber/RequestBomber/DeviceData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestBomber
+{
+    class DeviceData
+    {
+        public DeviceData(string device, string data, string time)
+        {
+            this.device = device;
+            this.data = data;
+            this.time = time;
+
+        }
+
+        public string device;
+        public string data;
+        public string time;
+    }
+}
