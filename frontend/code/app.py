0693b03e2d26880941965ebc1f3d264f43eb8d0e
Jacob Søndergaard <24315940+Jacob6565@users.noreply.github.com>
2019-11-28 16:25:17 +0100

    Nu burde det hele gerne virke igen.

--- frontend/code/app.py
+++ frontend/code/app.py
@@ -1,18 +1,16 @@
 import dash
 import DAL
 import dash_core_components as dcc
-import dash_daq as daq
 import dash_html_components as html
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output, State
 
 # Token valid for 50.000 request pr year.
-mapbox_access_token = 'pk.eyJ1Ijoid2F0ZXJib2lzIiwiYSI6ImNrMmFkaDhmejBmcGUzb3AyaGFucDMybGwifQ.YfZyC53gGwkDdi0CfnuJZw'
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
+mapbox_access_token = 'pk.eyJ1Ijoid2F0ZXJib2lzIiwiYSI6ImNrM2lna3B5cTA1YnYza3Q1ZGJwMHloZHUifQ.CpKwtHYASGSCYDwnZAmepA'
 app = dash.Dash("Water Bois")
 app.title = 'Waterbois'
 server = app.server
+
 
 def construct_default_graph():
     data_dict = {"time": [0, 1, 2, 3, 4, 5, 6, 7, 8]}
@@ -24,26 +22,28 @@ def construct_default_graph():
         fillcolor="#6897bb",
         mode='lines+markers'
     )
-    graph = (html.Div(dcc.Graph(
-        figure={'data': [data],
-                'layout': go.Layout(xaxis=dict(range=[min(data_dict["time"]), max(data_dict["time"])]),
-                                    yaxis=dict(range=[0, 10]),
-                                    margin={'l': 50, 'r': 1, 't': 100, 'b': 50},
-                                    title='{}'.format('None Selected'))}
-    )))
+    graph = {'data': [data], 'layout': go.Layout(
+        xaxis=dict(range=[min(data_dict["time"]), max(data_dict["time"])]),
+        yaxis=dict(range=[0, 10]),
+        margin={'l': 50, 'r': 10, 't': 50, 'b': 100})
+             }
+
     return graph
 
 
 all_probes_names = DAL.get_names_of_things()
+all_probes = DAL.get_things_objects()
+lats = DAL.get_latitudes(all_probes)
+lons = DAL.get_longitudes(all_probes)
 
 
 def create_defaultmap():
     fig = go.Figure(go.Scattermapbox(
-        lat=DAL.get_latitudes(all_probes),
-        lon=DAL.get_longitudes(all_probes),
+        lat=lats,
+        lon=lons,
         mode='markers',
         marker=go.scattermapbox.Marker(
-            size=11,
+            size=9,
             color='red'
         ),
         selected={
@@ -60,7 +60,7 @@ def create_defaultmap():
         autosize=True,
         hovermode='closest',
         clickmode='event+select',
-        margin=dict(l=40, r=10, t=10,b=10),
+        margin=dict(l=40, r=10, t=10, b=10),
         mapbox=go.layout.Mapbox(
             accesstoken=mapbox_access_token,
             bearing=0,
@@ -76,10 +76,7 @@ def create_defaultmap():
     return fig
 
 
-all_probes = DAL.get_things_objects()
-
 defaultmap = create_defaultmap()
-
 # these global variables are only initialized
 # on page load, so only once. So no worry
 # for it re-computing them each time a user performs an action.
@@ -101,7 +98,7 @@ main_page = html.Div([
         html.Div([
             dcc.Checklist(
                 id='checklist',
-                #options=[{'label': desc, 'value': name} for name, desc in zip(all_probes_names, DAL.get_descriptions(all_probes))],
+                # options=[{'label': desc, 'value': name} for name, desc in zip(all_probes_names, DAL.get_descriptions(all_probes))],
                 options=[{'label': name, 'value': name} for name in all_probes_names],
                 labelStyle={'display': 'block'},
             )
@@ -116,43 +113,55 @@ main_page = html.Div([
 
     html.Div([
         html.Div([
-            html.H4('Specify Interval', style={'textAlign': 'center', 'horinzontal-align': 'middle'}),
-            html.H5('Start', style={'textAlign': 'center', 'horizontal-align': 'middle', 'width': '100%'}),
+            html.H3('Select data to diplay', style={'textAlign': 'center', 'horizontal-align': 'middle',
+                                                    'font-family': 'Helvetica'}),
+            html.Div([dcc.Dropdown(id='data-elements', placeholder="Select measurement", )]),
+            html.H4('Specify Interval', style={'textAlign': 'center', 'horinzontal-align': 'middle', 'marginTop': 20,
+                                               'font-family': 'Helvetica'}),
+            html.H5('Start', style={'textAlign': 'center', 'horizontal-align': 'middle', 'width': '100%',
+                                    'font-family': 'Helvetica'}),
             html.Div(dcc.Input(
                 id="start_date",
                 type="text",
-                placeholder="yyyy-mm-dd"
+                placeholder="yyyy-mm-dd-hh-mm-ss",
+                style={'padding': '10px', 'border-radius': '4px', 'border': '1px solid #bbb'}
             ), style={'textAlign': 'center', 'horizontal-align': 'middle'}),
-            html.H5('End', style={'textAlign': 'center', 'horizontal-align': 'middle', 'width': '100%'}),
+            html.H5('End', style={'textAlign': 'center', 'horizontal-align': 'middle', 'marginTop': 15, 'width': '100%',
+                                  'font-family': 'Helvetica'}),
             html.Div(dcc.Input(
                 id="end_date",
                 type="text",
-                placeholder="yyyy-mm-dd"
+                placeholder="yyyy-mm-dd-hh-mm-ss",
+                style={'padding': '10px', 'border-radius': '4px', 'border': '1px solid #bbb'}
             ), style={'textAlign': 'center', 'horizontal-align': 'middle'}),
-            html.Button('Apply', id='date_button', style={'width': '100%', 'marginTop': 20}),
-            html.Div(id='updateresponse', style={'textAlign': 'center', 'marginTop': 20})
+            html.Button('Apply', id='date_button', style={'width': '100%', 'marginTop': 15,
+                                                          'padding': '10px', 'background-color': '#FFF',
+                                                          'border-radius': '4px', 'border': '1px solid #bbb'}),
+            html.Div(id='updateresponse', style={'textAlign': 'center', 'marginTop': 20, 'font-family': 'Helvetica'})
         ]),
     ], style={'width': '15%', 'display': 'inline-block', 'vertical-align': 'top'}),
 
     html.Div([
-        html.Div(id='graphs', children=default_graph),
-        html.H4('Select data to diplay:'),
-        html.Div([dcc.Dropdown(id='data-elements', )])
+        dcc.Graph(id='graph', figure=default_graph)
     ], style={'display': 'inline-block', 'width': '85%', 'marginBottom': 40}),
-])
+], style={'marginLeft': 10})
 
 admin_page = html.Div([
-    html.Div(dcc.Input(id="probeId", placeholder="Enter probe id", type="text"),
-             style={'margin': 20}),
-    html.Div(dcc.Input(id="lat", placeholder="Enter latitude", type="text"),
-             style={'margin': 20}),
-    html.Div(dcc.Input(id="lon", placeholder="Enter longitude", type="text"),
-             style={'margin': 20}),
-    html.Div(dcc.Input(id="description", placeholder="Enter decription of location", type="text"),
-             style={'margin': 20}),
-    html.Div(html.Button('Update location', id='upd_loc_btn'), style={'margin': 20}),
-    html.Div(id='response')
-], style={'width': '100%'})
+    html.H3('Update location of probe', style={'horizontal-align': 'middle',
+                                               'font-family': 'Helvetica'}),
+    html.Div(dcc.Input(id="probeId", placeholder="Enter probe id", type="text",
+             style={'width': '20%', 'padding': '10px', 'border-radius': '4px', 'border': '1px solid #bbb'})),
+    html.Div(dcc.Input(id="lat", placeholder="Enter latitude", type="text",
+             style={'marginTop': 20,'width':'20%', 'padding': '10px', 'border-radius': '4px', 'border': '1px solid #bbb'})),
+    html.Div(dcc.Input(id="lon", placeholder="Enter longitude", type="text",
+             style={'marginTop': 20, 'width':'20%', 'padding': '10px', 'border-radius': '4px', 'border': '1px solid #bbb'})),
+    html.Div(dcc.Input(id="description", placeholder="Enter description of location", type="text",
+             style={'marginTop': 20, 'width':'20%', 'padding': '10px', 'border-radius': '4px', 'border': '1px solid #bbb'})),
+    html.Div(html.Button('Update location', id='upd_loc_btn', style={'marginTop': 15, 'marginLeft':10, 'width':'20%',
+                                                                     'padding': '10px', 'background-color': '#FFF',
+                                                                     'border-radius': '4px', 'border': '1px solid #bbb'})),
+    html.Div(id='response', style={'marginTop': 20, 'font-family': 'Helvetica'})
+], style={'marginLeft': 20})
 
 
 @app.callback(
@@ -164,9 +173,14 @@ admin_page = html.Div([
      State('description', 'value')]
 )
 def patch_probe_location(n_clicks, probe_id,
-                          lat, lon, desc):
-    return 'Input: Id: {} Lat: {} Lon: {} Desc: {}'.format(
-        probe_id, lat, lon, desc)
+                         lat, lon, desc):
+    if n_clicks is None:
+        return " "
+    success = DAL.patch_location_of_thing(probe_id, lat, lon, desc)
+    if success:
+        return "Probe: {} was updated".format(probe_id)
+    else:
+        return "Something went wrong =/"
 
 
 @app.callback(Output('page-content', 'children'),
@@ -188,12 +202,16 @@ def update_data_elements(selected_probes):
     else:
         # a list of datastreams which is also a list
         list_of_datastreams = DAL.get_datastreams_of_things(selected_probes)
-        elements = []
+        names = []
+        descriptions = []
         for datastreams in list_of_datastreams:
             for datastream in datastreams:
-                elements.append(datastream['name'])
-        return [{'label': s, 'value': s}
-                for s in elements]
+                names.append(datastream['name'])
+                descriptions.append(datastream['description'])
+
+        zipped = zip(names, descriptions)
+        return [{'label': name, 'value': desc}
+                for desc, name in zipped]
 
 
 def construct_data_dict(observations, data_name):
@@ -202,7 +220,11 @@ def construct_data_dict(observations, data_name):
     data = []
     for observation in observations:
         for x in observation:
-            time.append(x['resultTime'][:10])
+            temp_time = x['resultTime'][:19]
+            day = temp_time[:10]
+            hour = temp_time[11:19]
+            total = day + '-' + hour
+            time.append(total)
             data.append(x['result'])
 
     data_dict['time'] = time
@@ -244,14 +266,13 @@ def update_map(value):
 
 
 @app.callback(
-    [Output('graphs', 'children'),
+    [Output('graph', 'figure'),
      Output('updateresponse', 'children')],
     [Input('data-elements', 'value'),
-     Input('checklist', 'value'),
      Input('date_button', 'n_clicks')],
     [State('start_date', 'value'),
      State('end_date', 'value')])
-def update_graph_data(datastream_name, selected_probes, n_clicks, start_date, end_date):
+def update_graph_data(datastream_name, n_clicks, start_date, end_date):
     response = ""
 
     # default graph, when nothing is selected.
@@ -261,22 +282,35 @@ def update_graph_data(datastream_name, selected_probes, n_clicks, start_date, en
         # med, da ellers ville det ikke være nogen graf i starten. Men det er også
         # bedre, da den kun bliver kaldt ved page-load og ikke hvergang brugeren
         # gør noget, hvilket denne funktion gør.
-        return default_graph, " "
+        return default_graph, response
     else:
         observations = DAL.get_observations_of_datastreams([datastream_name])
         data_dict = construct_data_dict(observations, datastream_name)
-        y_min = min(data_dict[datastream_name])
-        y_max = max(data_dict[datastream_name])
-        x_min = min(data_dict["time"])
-        x_max = max(data_dict["time"])
         if start_date is not None and \
                 end_date is not None:
             if start_date > end_date:
                 response = "Start date must be before end date."
+            elif start_date < data_dict["time"][0] or \
+                    end_date > data_dict["time"][-1]:
+                response = "Interval is out of bounds."
             else:
-                x_min = start_date
-                x_max = end_date
+                new_times = []
+                new_values = []
+                zipped = zip(data_dict["time"], data_dict[datastream_name])
+                for time, value in zipped:
+                    if time >= end_date:
+                        break
+                    elif time >= start_date:
+                        new_times.append(time)
+                        new_values.append(value)
+                data_dict["time"] = new_times
+                data_dict[datastream_name] = new_values
                 response = "Applied time interval to graph."
+
+        y_min = min(data_dict[datastream_name])
+        y_max = max(data_dict[datastream_name])
+        x_min = min(data_dict["time"])
+        x_max = max(data_dict["time"])
 
         data = (go.Scatter(
             x=list(data_dict["time"]),
@@ -286,13 +320,11 @@ def update_graph_data(datastream_name, selected_probes, n_clicks, start_date, en
             fillcolor="#6897bb"
         ))
 
-        graph = (html.Div(dcc.Graph(
-            figure={'data': [data], 'layout': go.Layout(xaxis=dict(range=[x_min, x_max]),
-                                                        yaxis=dict(range=[y_min - (y_min / 10), y_max + (y_max / 10)]),
-                                                        margin={'l': 50, 'r': 1, 't': 100, 'b': 50},
-                                                        title='Measurement: {}'.format(datastream_name))
-                    }
-        )))
+        graph = {'data': [data], 'layout': go.Layout(
+            xaxis=dict(range=[x_min, x_max]),
+            yaxis=dict(range=[y_min - (y_min / 10), y_max + (y_max / 10)]),
+            margin={'l': 50, 'r': 10, 't': 50, 'b': 100})
+                 }
 
         return graph, response
 
