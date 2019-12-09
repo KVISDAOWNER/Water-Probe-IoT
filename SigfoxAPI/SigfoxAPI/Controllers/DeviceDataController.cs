e94a27deb6a987ac6790d7ab645cb8f984eec082
Magnus <mwpe16@student.aau.dk>
2019-12-09 10:31:43 +0100

    Update DeviceDataController.cs

--- SigfoxAPI/SigfoxAPI/Controllers/DeviceDataController.cs
+++ SigfoxAPI/SigfoxAPI/Controllers/DeviceDataController.cs
@@ -66,7 +66,7 @@ namespace IO.Swagger.Controllers
         public virtual IActionResult NewData([FromBody]DeviceData body)
         {
             if (HelperFunctions.NewData(body))
-                return StatusCode(200, "{\"1D95A5\":{\"downlinkData\": \"f" + HelperFunctions.response.ToLower() + "\"}}");
+                return StatusCode(200, "{\"1D95A5\":{\"downlinkData\":\"f" + HelperFunctions.response.ToLower() + "\"}}");
             else
                 return StatusCode(404, default(Sample));
         }
